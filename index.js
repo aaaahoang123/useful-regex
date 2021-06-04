3a3cb2bec85b4a85d33cfe5086113d4277172cae
Hoang Do <hoangdo.2194@gmail.com>
2021-06-04 14:22:41 +0700

    - Rename variable.

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 // Match cvo. and not match config|local.ERROR|exception
-const MATCH_STRING_AND_NOT_CONTAIN_ANOTHERS = /^(?!.*?(config|local\.ERROR|exception)).*\b(cvo\.)\b/g;
+const MATCH_STRING_AND_NOT_CONTAIN_ANOTHER = /^(?!.*?(config|local\.ERROR|exception)).*\b(cvo\.)\b/g;
+
+console.log(MATCH_STRING_AND_NOT_CONTAIN_ANOTHER.test('config(\'cvo'));
